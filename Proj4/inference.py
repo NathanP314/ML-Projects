222306bd3f3aa0c89985e9dea5487c8ec11f7789
Nathan Perez <nathanperez314@gmail.com>
2026-03-27 12:39:53 -0500

    Update input pipeline

--- Proj4/inference.py
+++ Proj4/inference.py
@@ -15,12 +15,17 @@ def ce_inference(data_path):
 def main():
     print("Would you like to test the MNIST model or C. Elegans Model? Enter 'M' or 'CE':")
     model_choice = input().strip().upper()
+    while model_choice not in ['M', 'CE']:
+        print("Invalid choice. Please enter 'M' for MNIST or 'CE' for C. Elegans:")
+        model_choice = input().strip().upper()
+        
     print("Please enter the absolute path to the folder for inference data:")
     data_path = input().strip()
+    
     if model_choice == 'M':
         mnist_inference(data_path)
     elif model_choice == 'CE':
         ce_inference(data_path)
-        
+            
 if __name__ == "__main__":
     main()
